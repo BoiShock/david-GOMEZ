f9b6b7d0b6e4ab0fa1ec58a1fb071878c9cfce78
BoiShock <106532831+BoiShock@users.noreply.github.com>
2023-03-25 08:11:50 +0000

    Feed

---
+++ src/app/home/home.component.spec.ts
@@ -0,0 +1,13 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HomeComponent } from './home.component';
+
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HomeComponent ]
+    })
+    .compileComponen
