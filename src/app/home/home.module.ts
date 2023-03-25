4d55aedce77c01aad2b1f4bc7760784984645607
BoiShock <106532831+BoiShock@users.noreply.github.com>
2023-03-25 19:15:19 +0000

    Add some aliases

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,4 +6,34 @@ import { HomeComponent } from './home.component';
 import { StockValueComponent } from './stock-value/stock-value.component';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
-import { MatTableModule } 
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    StockValueComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatAutocompleteModule,
+    MatButtonModule
+    
+  ]
+  ,
+  exports:[MatButtonModule]
+})
+export class HomeModule { }
